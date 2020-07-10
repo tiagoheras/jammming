9dcd748bf5e612a6ce327ae2b958e9a8f08f552f
Tiago Heras <65797374+werduk@users.noreply.github.com>
2020-07-09 21:24:32 -0300

    Updates

---
+++ precache-manifest.5fd13f9c20b1754ed35f3db3052040bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e7b3395d0e0dd03a8e5a13b9a950c0",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "c8cfcc7cbb7dc7b37575",
+    "url": "/jammming/static/css/main.e8d947d6.chunk.css"
+  },
+  {
+    "revision": "b0f597be5bf5280f10d7",
+    "url": "/jammming/static/js/2.87f56ebd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jammming/static/js/2.87f56ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8cfcc7cbb7dc7b37575",
+    "url": "/jammming/static/js/main.f3107d07.chunk.js"
+  },
+  {
+    "revision": "d54c33d011ceab8fe7ea",
+    "url": "/jammming/static/js/runtime-main.af6152f4.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
